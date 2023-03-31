19ec25a5531832ebec451d54c326710a6af0b818
Maximilian H <lorganan@gmail.com>
2023-03-31 15:49:07 +0200

    agencies seems to be working, but giving out bad data

--- agencies.py
+++ agencies.py
@@ -13,8 +13,7 @@ file_path = path.join(dir, "agencies_manual_fix.csv")
 if not file_path:
     df_consult_expand = pd.json_normalize(df_consult["clientOrganizations"])
     df_consult_expand.to_csv(path.join(dir, "agencies_manual_fix.csv"), sep = ";")
-else:
-    df_consult_expand = pd.read_csv(file_path, sep = ";")
+df_consult_expand = pd.read_csv(file_path, sep = ";")
 df = df_consult.join(df_consult_expand)
 
 column_list = df.columns.tolist()
@@ -22,23 +21,46 @@ target_column_list = []
 for column in column_list:
     if column.isdigit():
         target_column_list.append(column)
+contract_dict = {}
 for index, row in df.iterrows():
-    number = row["account.registerNumber"]
+    register_number = row["account.registerNumber"]
+    print("Contractor:", register_number)
     if row["clientIdentity"] == "ORGANIZATION":
         name = str(row["lobbyistIdentity.name"])
     else:
-        name = str(row["lobbyistIdentity.commonFirstName"]), str(row["lobbyistIdentity.lastName"])
-        name = " ".join(name)
-    print(number, name)
+       name = str(row["lobbyistIdentity.commonFirstName"]), str(row["lobbyistIdentity.lastName"])
+       name = " ".join(name)
+    contractor_list = []
     for x in target_column_list:
+        contractor_list.append(cell_reader)
         if row[str(x)]:
+            print("Columns exist:", name, x)
             result = str(row[str(x)])
             if result not in ["None", "nan"]:
-                json_string = row[str(x)].replace("'s ", "s ").replace("O'R", "OR").replace("\'", "\"").replace("\\xa0", "")
+                print("Result not excluded:", name, x)
+                json_string = result.replace("'s ", "s ").replace("O'R", "OR").replace("\'", "\"").replace("\\xa0", "")
                 try:
                     tbn = json.loads(json_string)
                     df_sub = pd.json_normalize(tbn)
-                    name = df_sub["name"].values
-                    print(name)
-                except:
-                    print("Nope")
+                    # If it has a working reference, get that
+                    if "clientReferenceUrl" in df_sub.columns.tolist():
+                        contractor_register_number = str(df_sub["clientReferenceUrl"].values).split("suche/")[1].strip("']")
+                        print("-", contractor_register_number)
+                        contractor_list.append(contractor_register_number)
+                    # Otherwise, try to find the name
+                    else:
+                        contractor_name = re.sub(r"['\[\]]", "", str(df_sub["name"].values))
+                        # df names will need to be changed when incorporated into wrangling
+                        retrieved_index_array = df_full.loc[df_full["lobbyistIdentity.name"].str.contains(contractor_name, na = False) == True].index.values
+                        if retrieved_index_array.size > 0:
+                            retrieved_index = int(re.sub(r"\[|\]", "", str(retrieved_index_array)))
+                        if retrieved_index in locals():
+                            contractor_register_number = df_full.loc[retrieved_index]["registerNumber"].values.astype(str)[0]
+                            del retrieved_index
+                            print("-", contractor_register_number)
+                            contractor_list.append(contractor_register_number)
+                except Exception as e:
+                    x = 1
+                    #print(e)
+    contract_dict[register_number] = contractor_list
+print(contract_dict)
